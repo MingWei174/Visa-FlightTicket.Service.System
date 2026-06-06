07cf306cac771bea89d63bba82cdc305ce79951f
Ming Wei <w45166148@gmail.com>
2026-06-06 19:22:29 +0800

    chore: set base path to repository name in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Visa-FlightTicket.Service.System/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
